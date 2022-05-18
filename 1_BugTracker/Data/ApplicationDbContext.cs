453cbfcd5881fce5e365dbe32eec675da9b5c25a
ojr-dev <55997707+ojr-dev@users.noreply.github.com>
2022-05-18 16:26:34 -0400

    Add project files.

---
+++ 1_BugTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using _1_BugTracker.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace _1_BugTracker.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BT_User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+        }
+
+
+    }
+}
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<BT_User>
+{
+    public void Configure(EntityTypeBuilder<BT_User> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(60);
+        builder.Property(u => u.LastName).HasMaxLength(60);
+  
+    }
+}
---
+++ 1_BugTracker/Models/BT_User.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace _1_BugTracker.Models
+{
+    //identity user helps with user accounts
+    public class BT_User : IdentityUser
+    {
+        //display -> not for database, on view will be displayed as First Name
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        //notmapped tells database to not create a field for this in table
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+    }
+}
